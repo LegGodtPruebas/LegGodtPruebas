d468f0a468e1dc27d076ffb66ee2ed5b4ea18bbe
LegGodtPruebas <javiersalinas69x100@gmail.com>
2024-05-13 23:42:28 -0400

    Add files via upload

--- src/update.js
+++ src/update.js
@@ -1,27 +1,67 @@
-## Hello World 👋 I'm Tom
+const path = require("path");
+const fetch = require("node-fetch");
+const fs = require("fs");
+
+let stars = 0,
+  page = 1;
+let special;
+
+const CountStars = async () => {
+  let StarsData = await fetch(
+    `https://api.github.com/users/wodosharalatn/starred?per_page=100&page=${page}`
+  ).then((res) => res.json());
+  stars += StarsData.length;
+  page++;
+  if (StarsData.length === 100) CountStars();
+  else WriteReadMe();
+};
+
+const WriteReadMe = async () => {
+  //Get ReadMe path
+  const ReadMe = path.join(__dirname, "..", "README.md");
+  const date = new Date();
+  
+ //Season Based Emoji
+ let dd = date.getDate(),
+ mm = date.getMonth() + 1;
+
+if (mm === 12) special = ["⛄", "❄", "🎄"];
+else if (mm === 6 && dd === 5) special = ["🎉", "🎈", "🎊"];
+
+//Fetching Info From Github API
+let UserData = await fetch("https://api.github.com/users/wodosharlatan").then(
+ (res) => res.json()
+);
+
+//Creating the text what we gonna save on ReadMe file
+const text = `## Hello World 👋 I'm Tom
 A Web Developer from the Czech Republic. I like to code web applications and explore the limits of my skills. I have worked on many projects in the past. Some of them are open-source, make sure to check them out.  
 
 Thanks for visiting my GitHub profile. Have a great day ahead!
 
-<h2 align="center"> 💫 About Me 💫</h2>
+<h2 align="center"> ${special ? special[0] : "💫"} About Me ${
+ special ? special[0] : "💫"
+}</h2>
 
-```js
+\`\`\`js
 const Tom = {
  FavoriteLanguage: Laravel,
  Learning: Java,
  ProjectInMind: Birthday Reminder,
- OpenedPullRequests: 84,
- TotalCommits: 3484,
- OpenedIssues: 22,
- Stars: 6,
+ OpenedPullRequests: {{ PULL_REQUESTS }},
+ TotalCommits: {{ COMMITS }},
+ OpenedIssues: {{ ISSUES }},
+ Stars: {{ STARS }},
  Repositories: {
-    Created: 18,
-    Contributed: 8
+    Created: {{ REPOSITORIES }},
+    Contributed: {{ REPOSITORIES_CONTRIBUTED_TO }}
  },
 }; // My stats as an Epic Object ✨
-```
+\`\`\`
 
-<h2 align="center"> 🚀 My Stats 🚀</h2>
+<h2 align="center"> ${special ? special[1] : "🚀"} My Stats ${
+ special ? special[1] : "🚀"
+}</h2>
 
 <div align="center">
 <img src="https://github-readme-streak-stats.herokuapp.com/?user=wodosharlatan&theme=dark&hide_border=false">
@@ -52,7 +92,41 @@ const Tom = {
 
 <h2 align="center"> ⚙️ Last Updated ⚙️ </h2>
 
-<!-- Last updated on Tue May 14 2024 01:08:16 GMT+0000 (Coordinated Universal Time) ;-;-->
+<!-- Last updated on ${date.toString()} ;-;-->
 <div align="center">
-<i>Last updated on 14th May 2024 using Samsung Smart Refrigerator</i> 🧊 
+<i>Last updated on ${date.getDate()}${
+ date.getDate() === 1
+   ? "st"
+   : date.getDate() === 2
+   ? "nd"
+   : date.getDate() === 3
+   ? "rd"
+   : "th"
+} ${
+ [
+   "January",
+   "February",
+   "March",
+   "April",
+   "May",
+   "June",
+   "July",
+   "August",
+   "September",
+   "October",
+   "November",
+   "December",
+ ][date.getMonth()]
+} ${date.getFullYear()} using Samsung Smart Refrigerator</i> ${
+ special ? special[2] : "🧊"
+} ${mm === 6 && dd === 5 ? "and... today is my birthday" : ""}
 </div>
+`;
+
+//Saving on readme.md
+fs.writeFileSync(ReadMe, text);
+};
+
+(() => {
+    CountStars();
+})()
